5526e773a3ea75f0e4a34cd691ccddd1a5c536d5
jiushiboy <1302439162@qq.com>
2021-03-03 15:30:57 +0800

    stream消息驱动

--- cloud-stream-rabbitmq-provider8801/src/main/java/com/jiushiboy/springcloud/StreamMQMain8801.java
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/jiushiboy/springcloud/StreamMQMain8801.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @Author sh
+ * @Author 1450495810
  * @Date 2021/3/1 15:08
  */
 @SpringBootApplication
