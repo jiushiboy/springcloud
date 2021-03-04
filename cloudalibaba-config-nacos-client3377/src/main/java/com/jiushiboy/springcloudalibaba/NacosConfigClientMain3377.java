10292ff5245c16221dc2b8098efe2e369d7f2204
jiushiboy <1302439162@qq.com>
2021-03-04 15:08:35 +0800

    Nacos配置中心Config

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/jiushiboy/springcloudalibaba/NacosConfigClientMain3377.java
@@ -0,0 +1,17 @@
+package com.jiushiboy.springcloudalibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author 1450495810
+ * @Date 2021/3/4 14:54
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
